7e9b1a446fdfcc998743346983a22df0d4cfb380
LTPht <phatlt2203@gmail.com>
2024-03-11 18:06:06 +0700

    migration+models

--- EduPost/Data/ApplicationDbContext.cs
+++ EduPost/Data/ApplicationDbContext.cs
@@ -9,5 +9,10 @@ namespace EduPost.Data
             : base(options)
         {
         }
+        public DbSet<EduPost.Models.User>? User { get; set; }
+        public DbSet<EduPost.Models.Role>? Role { get; set; }
+        public DbSet<EduPost.Models.Article>? Article { get; set; }
+        public DbSet<EduPost.Models.Faculty>? Faculty { get; set; }
+        public DbSet<EduPost.Models.Status>? Status { get; set; }
     }
 }
