6cc8eb1a289d2eea36223fbd7dc443643576c4cb
LTPht <phatlt2203@gmail.com>
2024-03-11 16:32:38 +0700

    models changes

---
+++ EduPost/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduPost.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("role_id")]
+        public int? Role_id { get; set; }
+
+        [Column("roleName")]
+        [Required]
+        public string? RoleName { get; set; }
+    }
+}
