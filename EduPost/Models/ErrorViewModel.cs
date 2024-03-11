d493c8e15785f5547bf74d533802c08738bea61e
LuongTriQuan <quanltgcs210374@fpt.edu.vn>
2024-03-11 13:19:37 +0700

    Add project files.

---
+++ EduPost/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EduPost.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
