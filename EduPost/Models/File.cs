f251dbd7c2742ef486c15fd8c3ec68c542d3bdae
LuongTriQuan <quanltgcs210374@fpt.edu.vn>
2024-03-26 08:49:43 +0700

    File and Image models

---
+++ EduPost/Models/File.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduPost.Models
+{
+    public class File
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("file_id")]
+        public int? FileId { get; set; }
+
+        [Column("file_name")]
+        public string? FileName { get; set; }
+
+        [Column("file_path")]
+        public string? FilePath { get; set; }
+
+        public int? ArticleId { get; set; }
+    }
+}
