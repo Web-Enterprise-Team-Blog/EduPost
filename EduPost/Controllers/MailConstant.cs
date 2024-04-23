ad22ae24ba160abe8ac730abfe87ba4cbbfc8a77
Acer <kn78594@gmail.com>
2024-04-23 13:56:52 +0700

    Updated more UI in ResetPassword Page

--- EduPost/Controllers/MailConstant.cs
+++ EduPost/Controllers/MailConstant.cs
@@ -4,7 +4,7 @@
     {
         public static string hostEmail = "smtp.gmail.com";
         public static int portEmail = 587;
-        public static string emailSender = "khangphggcs210702@fpt.edu.vn";
-        public static string passwordSender = "tjvh jazo onwk tgoq";
+        public static string emailSender = "";
+        public static string passwordSender = "";
     }
 }
